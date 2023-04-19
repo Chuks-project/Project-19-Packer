076a5adf587e232ab9c5360e64a07c52f174b471
Chuks-project <chuksy237@yahoo.com>
2023-04-19 10:18:30 +0100

    new providers

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,31 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
-
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "~> 3.71"
-#     }
-#   }
-# }
-
-# # Configure the AWS Provider
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-
-# terraform {
-#  required_providers {
-#  aws = {
-#  source = "hashicorp/aws"
-#  version = "~> 3.71"
-#  }
-#  }
-# }
-
-# provider "aws" {
-#   region = var.region
-# }
