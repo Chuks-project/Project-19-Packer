845bcca84c8a3e09fd2133e4a97109b6f29d2d21
Chuks-project <chuksy237@yahoo.com>
2023-04-20 21:14:37 +0100

    provi

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,12 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "vic-new-terraform-bucket"
-#     key            = "global/s3/terraform.tfstate"
-#     region         = "us-east-1"
-#     dynamodb_table = "terraform-locks"
-#     encrypt        = true
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "vic-new-terraform-bucket"
+    key            = "global/s3/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 
 terraform {
   backend "remote" {
