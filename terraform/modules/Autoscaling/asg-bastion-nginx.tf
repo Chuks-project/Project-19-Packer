665e9b89bf82379abfa06c064e69d12375a5d2a5
Chuks-project <chuksy237@yahoo.com>
2023-04-26 21:36:05 +0000

    changes

--- terraform/modules/Autoscaling/asg-bastion-nginx.tf
+++ terraform/modules/Autoscaling/asg-bastion-nginx.tf
@@ -88,11 +88,11 @@ resource "aws_autoscaling_group" "nginx-asg" {
     propagate_at_launch = true
   }
 
-
 }
 
-# attaching autoscaling group of nginx to external load balancer
+ # attaching autoscaling group of nginx to external load balancer
 resource "aws_autoscaling_attachment" "asg_attachment_nginx" {
   autoscaling_group_name = aws_autoscaling_group.nginx-asg.id
   alb_target_group_arn   = var.nginx-alb-tgt
 }
+
